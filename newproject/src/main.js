67da6675b2463d128ea693fd7d8b0457a51d27e0
Petr Smirnov <120395181+Petr1221@users.noreply.github.com>
2022-12-19 13:10:08 +0000

    cd

--- newproject/src/main.js
+++ newproject/src/main.js
@@ -2,3 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+import * as Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+const app = Vue.createApp(App)
+
+app.use(VueAxios, axios)
+
+app.mount('#app')
